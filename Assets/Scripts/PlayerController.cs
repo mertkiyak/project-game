b4e716fa422d11b1d2b53bac536d7d0f9083ad7d
mertkiyak <mkiyak46@gmail.com>
2025-03-01 03:40:57 +0300

    char controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,74 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("References")]
+    [SerializeField] private Transform _orientationTransform;
+    
+    [Header("Setting")]
+    [SerializeField] private float _movementSpeed;
+
+    [Header("Jump Setting")]
+    [SerializeField] private KeyCode _jumpkey;
+    [SerializeField] private float _jumpForce;
+    [SerializeField] private bool _canJump;
+    [SerializeField] private float _jumpCooldown;
+
+    [Header("Geround Check")]
+    [SerializeField]private float _playerHeight;
+    [SerializeField] private LayerMask _groundLayer;
+
+
+    private Rigidbody _playerRigibody;
+    
+    private float _horizontalInput , _verticalInput;
+
+    private Vector3 _movementDirection;
+    
+
+    private void Awake()
     {
-        
+        _playerRigibody = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        SetInputs();
+    }
+    private void FixedUpdate()
+    {
+        SetPlayerMovement();
+    }
+    private void SetInputs()
+    {
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _verticalInput = Input.GetAxis("Vertical");
+
+        if (Input.GetKey(_jumpkey)&& _canJump && IsGrounded())
+        {
+            _canJump = false;
+            SetPlayerJumping();
+            Invoke(nameof(ResetJumping),_jumpCooldown);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetPlayerMovement()
     {
-        
+        _movementDirection = _orientationTransform.forward * _verticalInput  + _orientationTransform.right * _horizontalInput;
+        _playerRigibody.AddForce(_movementDirection.normalized * _movementSpeed, ForceMode.Force);
     }
+
+    private void SetPlayerJumping()
+    {
+        //_playerRigibody.linearVelocity = new Vector3(_playerRigibody.linearVelocity.x, 0f, _playerRigibody.linearVelocity.z);
+        _playerRigibody.AddForce(transform.up * _jumpForce, ForceMode.Impulse);
+
+    }
+    private void ResetJumping()
+    {
+        _canJump = true;
+    }
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, _playerHeight * 0.5f + 0.2f, _groundLayer);
+    }
+
 }
